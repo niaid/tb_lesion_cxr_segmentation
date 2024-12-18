3851334a7a659bd692349ae6096f132165f853f2
Kantipudi Karthik <karthikk2085@gmail.com>
2024-12-18 10:48:49 -0700

    Updated with binary mask threshold in arguments

--- segment_tb_cxr/auxiliary/ensemble_nnunet_yolov8m.py
+++ segment_tb_cxr/auxiliary/ensemble_nnunet_yolov8m.py
@@ -151,7 +151,10 @@ def gen_ensembled_yolov8_nnunet_segmentation(
         The binary segmentation mask is written to the output folder as a `_seg.nrrd` file.
 
     """
-    ensembled_prob = np.mean([yolov8_prob_map, nnunet_prob_map], axis=0)
+    if yolov8_prob_map is not None:
+        ensembled_prob = np.mean([yolov8_prob_map, nnunet_prob_map], axis=0)
+    else:
+        ensembled_prob = nnunet_prob_map / 2
 
     ensemble_nnunet_yolov8_prob_map_img = sitk.GetImageFromArray(ensembled_prob)
 
@@ -179,6 +182,9 @@ def main():
     )
     parser.add_argument(
         "output_seg_dir", type=str, help="output directory to save the images"
+    )
+    parser.add_argument(
+        "--binary_mask_threshold", type=float, default=0.5, help="Binary mask threshold"
     )
 
     args = parser.parse_args()
@@ -213,7 +219,11 @@ def main():
         yolov8_prob_map = gen_yolov8_prob_map(file, yolov8_model)
         nnunet_prob_map = gen_nnunet_prob_map(file, predictor)
         gen_ensembled_yolov8_nnunet_segmentation(
-            file, yolov8_prob_map, nnunet_prob_map, args.output_seg_dir
+            file,
+            yolov8_prob_map,
+            nnunet_prob_map,
+            args.output_seg_dir,
+            args.binary_mask_threshold,
         )
 
 
